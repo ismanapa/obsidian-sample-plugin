f1337dea016ba43e72a5320bcd8c39e398ed6e6f
Isma Navarro <ismanapa@gmail.com>
2023-09-19 16:51:11 +0200

    add pipeline

--- etc/version-bump.mjs
+++ etc/version-bump.mjs
@@ -9,6 +9,6 @@ manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+let versions = JSON.parse(readFileSync("etc/versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
